f862736e7fbd6d98a5946593569b764a52742ab9
iSrDxv <srclauyt@gmail.com>
2024-06-30 16:52:05 -0300

    test

---
+++ src/isrdxv/lobbycore/Loader.php
@@ -0,0 +1,27 @@
+<?php
+declare(strict_types=1);
+
+namespace isrdxv\lobbycore;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\SingletonTrait;
+
+class Loader extends PluginBase
+{
+    use SingletonTrait;
+
+    function onLoad(): void
+    {
+        
+    }
+
+    function onEnable(): void
+    {
+        
+    }
+
+    function onDisable(): void
+    {
+        
+    }
+}
